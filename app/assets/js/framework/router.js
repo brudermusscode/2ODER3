1fbbc5b65b541c6555e55b1521e6f6a9ccfb3d09
ram <ram@bruder.business>
2026-05-26 18:54:53 +0200

    Neueeeee Bilderrrr

--- app/assets/js/framework/router.js
+++ app/assets/js/framework/router.js
@@ -29,7 +29,7 @@ export const routes = {
     mark: "login",
     hide_sidebar: true,
     background: {
-      image: "/jesus-login.png",
+      image: "/jesus-php.png",
       blur: 0,
       color: "rgba(0,0,0,.24)",
     },
