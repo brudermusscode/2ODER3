15ac9f75fd34ca690cc21527bf9047849cd87e45
brudermusscode <justin@heia.kim>
2026-05-30 19:52:22 +0200

    Fix Player Misaligned

--- app/templates/home/index.php
+++ app/templates/home/index.php
@@ -13,61 +13,14 @@ use Bruder\Model\Log;
   # + Coding Session.
   include __DIR__ . "/_coding-session.php" ?>
 
+
   <div fl fldircol gap=smol+>
     <p text smol bold ttup color=tertiary pinline12>Aktuellste Devlogs</p>
 
-    <?php foreach (
-      Project::with(["logs" => function ($q) {
-        $q->limit(4)
-          ->orderBy("created_at", "DESC");
-      }])
-        ->withCount("logs")
-        ->get() as $Project
-    ) : ?>
+    <?php
 
-      <?php if ($Project->logs_count) : ?>
-        <div>
-          <div pb12 fl alic jucsb>
-            <div fl alic gap=smol>
-              <div circled window style="height:38px;width:38px;" fl alic jucc>
-                <mi color=tertiary midler>deployed_code</mi>
-              </div>
-
-              <p text mid bold><?= $Project->name ?></p>
-            </div>
-
-            <a fl alic gap=smol href="<?= $Project->logs->first()?->link() ?? $Project->link() ?>">
-              <div background=quadro color=quadro-text rounded=smolplus pinline10 pblock6>
-                <p text smol semibold ttup>
-                  <strong><?= $Project->logs_count ?></strong> logs
-                </p>
-              </div>
-
-              <mi>arrow_forward</mi>
-            </a>
-          </div>
-
-          <div fl alic gap=smol>
-            <?php
-
-            /**
-             * @var Collection<Log>
-             */
-            $Logs = $Project->logs;
-
-            foreach ($Logs as $key => $Log) : ?>
-              <div style="flex-basis:25%;max-width:25%;">
-                <?php include TEMPLATE . "/log/_log.php" ?>
-              </div>
-            <?php endforeach;
-
-            unset($Logs);
-
-            ?>
-          </div>
-        </div>
-      <?php endif; ?>
-    <?php endforeach ?>
+    # + All latest devlogs.
+    include __DIR__ . "/_latest-devlogs-all.php"; ?>
   </div>
 
 
@@ -86,6 +39,13 @@ use Bruder\Model\Log;
   <div fl fldircol gap=smol+>
     <p text smol bold ttup color=tertiary pinline12>Feature Tracking</p>
 
+    <div style="position:sticky;top:36px;" z window p24>
+      <p text fl alic jucc gap=smol+ tac color=primary>
+        <mi>waving_hand</mi>
+        Diese Seite ist noch laaange nicht fertig.
+      </p>
+    </div>
+
     <div fl jucsb flex-wrap>
       <?php foreach ($features as $feature => $done) : ?>
         <div background=hover-dark rounded=stdplus pblock42 tac slight mb12
@@ -103,13 +63,4 @@ use Bruder\Model\Log;
 
 
 
-  <div style="position:sticky;top:36px;" z window p32>
-    <p text tac style="text-align: justify;">
-      <strong fl alic gap=smol mb8>
-        <mi>waving_hand</mi>
-        Diese Seite ist aktuell noch im Aufbau.
-      </strong>
-      Unten findest du einige Dinge, die ich plane zu implementieren. Grundidee ist, Dir den Fortschritt meiner Projekte zu präsentieren und Dir die Möglichkeit zu geben, persönlich daran teilzuhaben.
-    </p>
-  </div>
-  </div>
+</content>
